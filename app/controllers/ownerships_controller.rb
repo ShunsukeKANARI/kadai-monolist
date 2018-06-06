938904c3120cc57fd7efc31df17a958062431d48
shunsuke.kanari <kanari01git@gmail.com>
2018-06-06 13:20:21 +0000

    fic ownership_controller

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -28,8 +28,8 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
-    elsif params[:type] =='have'
-      current_user.unhave(@item)
+    elsif params[:type] =='Have'
+      current_user.unhave(@item) 
       flash[:success] = '商品の Have を解除しました。'
     end
 
